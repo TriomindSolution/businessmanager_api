3662b0a20f5e471562d1d4d047ff22d6ba1ef73b
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2024-03-04 01:32:32 +0600

    all code

---
+++ app/Http/Controllers/Backend/Category/Categorycontroller.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Category;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class Categorycontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Category::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->update($request->all());
+
+        return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $category = Category::findOrFail($id);
+        $category->delete();
+
+        return 204; 
+    }
+}
