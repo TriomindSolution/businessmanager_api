1a5e3e9309ec44e386a010ad4ba8679a965a9af5
sadik7375 <68754591+sadik7375@users.noreply.github.com>
2024-03-21 00:49:33 +0600

    expense category,expense,customer done

---
+++ app/Http/Controllers/Backend/Customer/CustomerController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Customer;
+
+use Illuminate\Http\Request;
+use App\Traits\ResponseTrait;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use Illuminate\Database\QueryException;
+use App\Models\Customer;
+
+
+class CustomerController extends Controller
+{
+
+    use ResponseTrait;
+
+    public function customerList(Request $request)
+    {
+        $limit = $request->input('limit', 20);
+
+        $cusData = Customer::latest()->paginate($limit);
+
+
+        // not empty checking
+        if ($cusData->isEmpty()) {
+            $message = "No customer data found.";
+            return $this->responseError(403, false, $message);
+        }
+
+        $message = "Successfully data shown";
+        return $this->responseSuccess(200, true, $message, $cusData);
+    }
+
+
+
+    public function customerRetrieve($expenseId)
+    {
+        $cusData = Customer::where('id', $expenseId)->get();
+
+        // not empty checking
+        if ($cusData->isEmpty()) {
+            $message = "No Customer data found.";
+            return $this->responseError(403, false, $message);
+        }
+
+        $message = "Successfully data shown";
+        return $this->responseSuccess(200, true, $message, $cusData);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public function customerStore(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string',
+                'phone' => 'required|string',
+                'address_1'=>'required|string',
+                'address_2' => 'nullable|string',
+                'customer_code' => 'nullable|integer',
+                'order_count' => 'nullable|string',
+                'created_by' => 'nullable|string'
+
+
+            ]);
+
+            $data = [
+                'name' => $request->name,
+                'phone' => $request->phone,
+                'address_1'=>$request->address_1,
+                'address_2'=>$request->address_2,
+                'customer_code'=>mt_rand(100000, 999999),
+                'order_count'=>$request->order_count,
+                'created_by' => auth()->id()
+
+            ];
+
+            $customer = Customer::create($data);
+
+            $message = "Customer Created Successfully";
+            return $this->responseSuccess(200, true, $message, $customer); // Use responseSuccess method from the ResponseTrait
+        } catch (\Exception $e) {
+            \Log::error($e);
+            return $this->responseError(Response::HTTP_INTERNAL_SERVER_ERROR, false, $e->getMessage()); // Use responseError method from the ResponseTrait
+        }
+    }
+
+
+
+
+    public function customerUpdate(Request $request, $id)
+    {
+        $customerData =Customer::findOrFail($id);
+
+        try {
+            if ($customerData) {
+                $customerData->update([
+
+                    'name' => $request->name ?? $customerData->name,
+                    'phone' => $request->phone ??   $customerData->phone,
+                    'address_1'=>$request->address_1 ?? $customerData->address_1,
+                    'address_2'=>$request->address_2 ??   $customerData->address_2 ,
+
+
+
+
+                ]);
+
+                $message = "customer data has been updated";
+
+                return $this->responseSuccess(200, true, $message, $customerData);
+            }
+        } catch (QueryException $e) {
+            return $this->responseError(Response::HTTP_INTERNAL_SERVER_ERROR, false, $e->getMessage());
+        }
+    }
+
+
+
+
+    public function destroy($customerId)
+    {
+        try {
+            $customerData = Customer::findOrFail($customerId);
+            if ( $customerData) {
+                $customerData->delete();
+                $message = "Customer Deleted Succesfully";
+
+                return $this->responseSuccess(200, true, $message, []);
+            }
+        } catch (QueryException $e) {
+            return $this->responseError(Response::HTTP_INTERNAL_SERVER_ERROR, false, $e->getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Backend\Expense\ExpenseController;
 use App\Http\Controllers\Backend\Product\ProductController;
 use App\Http\Controllers\Backend\Categories\CategoriesController;
 use App\Http\Controllers\Backend\Expensecategory\ExpensecategoryController;
+use App\Http\Controllers\Backend\Customer\CustomerController;
 
 
 Route::group(["middleware" => ["api"]], function () {
@@ -41,13 +42,40 @@ Route::group(["middleware" => ["api"]], function () {
       Route::post('/expensecategory/store', 'expensecategoryStore');
       Route::put('/expensecategory/update/{expensecategory_id}', 'expensecategoryUpdate');
       Route::delete('/expensecategory/delete/{expensecategory_id}', 'destroy');
+      Route::get('/expensecategory-list', 'expenseCatgoeryList');
+      Route::get('/expensecategory-retrieve/{expensecategory_id}', 'expenseCategoryRetrieve');
+
+
      });
        //-------------------------------------expense--------------------------------------
     Route::controller(ExpenseController::class)->group(function () {
       Route::post('/expense/store', 'expenseStore');
       Route::put('/expense/update/{expense_id}', 'expenseUpdate');
       Route::delete('/expense/delete/{expense_id}', 'destroy');
+      Route::get('/expense-list', 'expenseList');
+      Route::get('/expense-retrieve/{expense_id}', 'expenseRetrieve');
+
+
+
+
      });
+
+        //-------------------------------------customer--------------------------------------
+        Route::controller(CustomerController::class)->group(function () {
+            Route::post('/customer/store', 'customerStore');
+            Route::put('/customer/update/{customer_id}', 'customerUpdate');
+            Route::delete('/customer/delete/{customer_id}', 'destroy');
+            Route::get('/customer-list', 'customerList');
+            Route::get('/customer-retrieve/{customer_id}', 'customerRetrieve');
+
+
+
+
+        });
+
+
+
+
 });
 
 });
